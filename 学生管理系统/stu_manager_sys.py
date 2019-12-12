f054e619f8a7bdaf1c4bba2c658380a94e8dabfb
1006er <14994937442@qq.com>
2019-12-12 20:24:55 +0800

    19/12/12 表格完善，update细节优化，（剩余模糊查询,差错检测）commit

--- 学生管理系统/stu_manager_sys.py
+++ 学生管理系统/stu_manager_sys.py
@@ -5,6 +5,7 @@ import codecs
 import sys
 import sqlite3
 import os
+import random
 
 app = Flask(__name__)
 app.secret_key="123456789"
@@ -22,6 +23,8 @@ def back():
         return render_template('manager.html', account=session['account'], name=session['name'], sex=session['sex']);
     elif(session['login_type']=='教师'):
         return render_template('teacher.html', account=session['account'], name=session['name'], sex=session['sex'],major=session['major']);
+    elif (session['login_type'] == '学生'):
+        return render_template('student.html',account=session['account'] , name=session['name'],sex = session['sex'],day = session['day'],class_name = session['class'] );
     else:
         return render_template('login.html');
 
@@ -56,18 +59,29 @@ def login():
         session['account']=result[0][0];
         session['name'] = result[0][1];
         session['sex'] = result[0][2];
+        session['password'] = result[0][3];
         session['login_type'] = "管理人员";
-        return render_template('manager.html',account=session['account'],name=session['name'],sex=session['sex']);
+        return render_template('manager.html',account=session['account'],name=session['name'],sex=session['sex'],password = session['password']);
 #检测是否为学生
     sql = "select * from student where id ='" + account + "'and password='" + password + "'";
     cur = conn.cursor()
     cur.execute(sql)
     result = cur.fetchall()  # 获得查询的结果
-    cur.close()
+    #cur.close()
     print(sql)
     print(result.__len__());
     if (result.__len__() != 0):
-        return render_template('student.html', data=result[0]);
+        session['account'] = result[0][0];
+        session['name'] = result[0][1];
+        session['sex'] = result[0][2];
+        session['day'] = result[0][3];
+        #session['class'] = result[0][4];
+        session['password'] = result[0][5];
+        session['login_type'] = "学生";
+        sql = "select name from class where id = '%s'"%(result[0][4]);
+        cur.execute(sql);
+        session['class'] = cur.fetchall()[0][0];
+        return render_template('student.html',account=session['account'] , name=session['name'],sex = session['sex'],day = session['day'],class_name = session['class'],password = session['password'] );
 #检测是否为教师
     sql = "select * from teacher where id ='" + account + "'and password='" + password + "'";
     cur = conn.cursor()
@@ -87,16 +101,20 @@ def login():
         major_list = cur.fetchall();
         major = major_list[0][0]
         session['major'] = major;
-        return render_template('teacher.html', account=session['account'],name = session['name'],sex = session['sex'],major = session['major'] );
+        return render_template('teacher.html', account=session['account'],name = session['name'],sex = session['sex'],major = session['major'],password = session['password'] );
 
     return render_template('login.html', message="用户名或密码错误");
 
 @app.route('/manager',methods=['GET'])
 def to_change_manager_inf():
-    return render_template('manager_chg_information.html',account=session['account'],name=session['name'],sex=session['sex']);
+    if 'login_type' not in session:
+        return render_template('login.html');
+    return render_template('manager_chg_information.html',account=session['account'],name=session['name'],sex=session['sex'],password = session['password']);
 
 @app.route('/stu_delete',methods=['GET'])
 def stu_delete():
+    if 'login_type' not in session:
+        return render_template('login.html');
     account = session['account'];
     name = session['name'];
     sex = session['sex'];
@@ -110,13 +128,15 @@ def stu_delete():
     conn.commit();
     #查询数据
     cur = conn.cursor()
-    cur.execute("select * from student");
+    cur.execute("select student.id,student.name,student.sex,student.birthday,class.name,password from student,class where class.id = student.class_num;");
     result = cur.fetchall()  # 获得查询的结果
     cur.close()
     return render_template('manager_stu_list.html',account=account,name=name,sex=sex,stu_table=result);
 
 @app.route('/m_student',methods=['GET'])
 def m_student():
+    if 'login_type' not in session:
+        return render_template('login.html');
     id = session['account'];
     name = session['name'];
     sex = session['sex'];
@@ -124,7 +144,7 @@ def m_student():
     database = "database\Stu_Message_manger.db"
     conn = sqlite3.connect(database)
     cur = conn.cursor()
-    cur.execute("select * from student");
+    cur.execute("select student.id,student.name,student.sex,student.birthday,class.name,password from student,class where class.id = student.class_num;");
     result = cur.fetchall()  # 获得查询的结果
     cur.close()
     return render_template('manager_stu_list.html',account=session['account'],name=session['name'],sex=session['sex'],stu_table=result);
@@ -132,6 +152,8 @@ def m_student():
 
 @app.route('/manager_change',methods=['GET'])
 def manager_change():
+    if 'login_type' not in session:
+        return render_template('login.html');
     account = request.args['new_account'];
     name = request.args['new_name'];
     password = request.args['new_password'];
@@ -148,6 +170,8 @@ def manager_change():
 
 @app.route('/to_stu_update',methods=['GET'])
 def to_stu_update():
+    if 'login_type' not in session:
+        return render_template('login.html');
     stu_id = request.args['stu_id'];
     sql =  "select * from student where id = '"+stu_id+"'";
     database = "database\Stu_Message_manger.db"
@@ -156,11 +180,18 @@ def to_stu_update():
     cur.execute(sql);
     result = cur.fetchall()  # 获得查询的结果
     result = result[0];
+    cur.execute("select name from class");
+    class_name = cur.fetchall();
+    sql = "select name from class where id = '%s'"%(result[4]);
+    cur.execute(sql);
+    class_name_n = cur.fetchall()[0][0];
     print(result);
-    return render_template('stu_update.html',result = result);
+    return render_template('stu_update.html',result = result,class_name = class_name,class_name_n = class_name_n);
 
 @app.route('/stu_update',methods=['GET'])
 def stu_update():
+    if 'login_type' not in session:
+        return render_template('login.html');
     new_account = request.args['new_account'];
     new_name = request.args['new_name'];
     new_sex = request.args['new_sex'];
@@ -169,23 +200,33 @@ def stu_update():
     new_password = request.args['new_password'];
     #修改数据库数据
     database = "database\Stu_Message_manger.db"
-    sql = "update student set name='%s',sex='%s',birthday='%s',class_num='%s',password='%s' where id = '%s'"%(new_name,new_sex,new_date,new_class,new_password,new_account);
-    print(sql);
     conn = sqlite3.connect(database)
     cur = conn.cursor()
+    #转换班级名-》班级号
+    sql = "select id from class where name = '%s'"%(new_class);
+    cur.execute(sql);
+    new_class = cur.fetchall()[0][0];
+    cur = conn.cursor();
+    #组建新的更新语句
+    sql = "update student set name='%s',sex='%s',birthday='%s',class_num='%s',password='%s' where id = '%s'" % (new_name, new_sex, new_date, new_class, new_password, new_account);
     cur.execute(sql);
     conn.commit();
     cur.close();
-
     #查询新的学生表
     cur = conn.cursor()
-    cur.execute("select * from student");
+    cur.execute("select student.id,student.name,student.sex,student.birthday,class.name,password from student,class where class.id = student.class_num;");
     result = cur.fetchall()  # 获得查询的结果
     cur.close()
-    return render_template('manager_stu_list.html', account=session['account'], name=session['name'],sex=session['sex'], stu_table=result);
+    if(session['login_type']=="管理人员"):
+        return render_template('manager_stu_list.html', account=session['account'], name=session['name'],sex=session['sex'], stu_table=result);
+    if(session['login_type']=="学生"):
+        return render_template('login.html');
+
 
 @app.route('/m_teacher',methods=['GET'])
 def m_teacher():
+    if 'login_type' not in session:
+        return render_template('login.html');
     id = session['account'];
     name = session['name'];
     sex = session['sex'];
@@ -200,6 +241,8 @@ def m_teacher():
 
 @app.route('/tea_delete',methods=['GET'])
 def tea_delete():
+    if 'login_type' not in session:
+        return render_template('login.html');
     account = session['account'];
     name = session['name'];
     sex = session['sex'];
@@ -220,6 +263,8 @@ def tea_delete():
 
 @app.route('/to_tea_update',methods=['GET'])
 def to_tea_update():
+    if 'login_type' not in session:
+        return render_template('login.html');
     tea_id = request.args['tea_id'];
     print('========================================'+tea_id);
     sql =  "select teacher.id,teacher.name,teacher.sex,major.name,teacher.password from teacher,major where teacher.major=major.id and teacher.id=%s"%(tea_id);
@@ -231,9 +276,74 @@ def to_tea_update():
     result = result[0];
     print(result);
     return render_template('tea_update.html',result = result);
+@app.route('/to_my_course',methods=['GET'])
+def to_my_course():
+    if 'login_type' not in session:
+        return render_template('login.html');
+    stu_id = request.args['stu_id'];
+    sql = "select course.id,course.name,teacher.name,grade.grade from teacher,course,grade,student where teacher.id = course.teacher_id and grade.student_id = student.id and grade.class_id = course.id and student.id = '%s';"%(stu_id);
+    database = "database\Stu_Message_manger.db"
+    conn = sqlite3.connect(database)
+    cur = conn.cursor()
+    cur.execute(sql);
+    result = cur.fetchall()  # 获得查询的结果
+    return render_template('my_course.html',result = result);
+@app.route('/to_add_my_course',methods=['GET'])
+def to_add_my_course():
+    if 'login_type' not in session:
+        return render_template('login.html');
+    stu_id = session['account'];
+    database = "database\Stu_Message_manger.db"
+    conn = sqlite3.connect(database)
+    cur = conn.cursor()
+    sql = "select grade.class_id from grade where student_id = '%s'"%(stu_id);
+    print(sql);
+    cur.execute(sql);
+    result = cur.fetchall();
+    list = "("
+    print("result"+str(result));
+    for id in result:
+        list = list+"'"+id[0]+"',"
+    list = list+"'XXXXX')"
+    print(list);
+    sql = "select course.*,teacher.name from course,teacher where course.teacher_id = teacher.id and course.id not in"+list;
+    cur.execute(sql);
+    result = cur.fetchall();
+    return render_template('add_my_course.html',table=result);
+
+@app.route('/add_stu_course',methods=['GET'])
+def add_stu_course():
+    if 'login_type' not in session:
+        return render_template('login.html');
+    stu_id = session['account'];
+    course_id = request.args['course_id'];
+    id = int(random.uniform(0,1000000));
+    sql = "INSERT INTO grade VALUES ('%s','%s','%s','0')"%(id,stu_id,course_id);
+    database = "database\Stu_Message_manger.db";
+    conn = sqlite3.connect(database)
+    cur = conn.cursor()
+    print("sql1=========="+sql)
+    cur.execute(sql);
+    conn.commit();
+    sql = "select course.stu_num from course where course.id ='%s' "%(course_id);
+    cur.execute(sql);
+    num =  cur.fetchall()[0][0];
+    num = int(num);
+    num = num+1;
+    sql = "update course set stu_num = '%s' where id = '%s'"%(num,course_id);
+    cur.execute(sql);
+    conn.commit();
+    print("sql2==========" + sql)
+    sql = "select course.id,course.name,teacher.name,grade.grade from teacher,course,grade,student where teacher.id = course.teacher_id and grade.student_id = student.id and grade.class_id = course.id and student.id = '%s';" %(stu_id);
+    cur.execute(sql);
+    result = cur.fetchall()  # 获得查询的结果
+    return render_template('my_course.html', result=result);
+
 
 @app.route('/tea_update',methods=['GET'])
 def tea_update():
+    if 'login_type' not in session:
+        return render_template('login.html');
     new_account = request.args['new_account'];
     new_name = request.args['new_name'];
     new_sex = request.args['new_sex'];
@@ -280,6 +390,8 @@ def tea_update():
         return render_template('login.html');
 @app.route('/m_class',methods=['GET'])
 def m_class():
+    if 'login_type' not in session:
+        return render_template('login.html');
     id = session['account'];
     name = session['name'];
     sex = session['sex'];
@@ -294,6 +406,8 @@ def m_class():
 
 @app.route('/class_delete',methods=['GET'])
 def class_delete():
+    if 'login_type' not in session:
+        return render_template('login.html');
     account = session['account'];
     name = session['name'];
     sex = session['sex'];
@@ -314,6 +428,8 @@ def class_delete():
 
 @app.route('/to_class_update',methods=['GET'])
 def to_class_update():
+    if 'login_type' not in session:
+        return render_template('login.html');
     class_id = request.args['class_id'];
     print("classid                     :" +class_id);
     sql =  "select course.id,course.name,teacher.name,course.max_num,course.stu_num,course.time,course.credit,course.start_term,course.class_type from course,teacher where course.teacher_id = teacher.id and course.id='%s'"%(class_id)
@@ -329,6 +445,8 @@ def to_class_update():
 
 @app.route('/class_update',methods=['GET'])
 def class_update():
+    if 'login_type' not in session:
+        return render_template('login.html');
     new_account = request.args['new_account'];
     new_name = request.args['new_name'];
     new_teacher = request.args['new_teacher'];
@@ -364,35 +482,57 @@ def class_update():
     return render_template('manager_class_list.html', account=session['account'], name=session['name'], sex=session['sex'], class_table=result);
 @app.route('/to_add_student',methods=['GET'])
 def to_add_student():
-    return render_template('add_student.html')
+    if 'login_type' not in session:
+        return render_template('login.html');
+
+    sql = "select name from class"
+    database = "database\Stu_Message_manger.db"
+    conn = sqlite3.connect(database)
+    cur = conn.cursor()
+    cur.execute(sql);
+    result = cur.fetchall()  # 获得查询的结果
+    return render_template('add_student.html',class_name = result)
 @app.route('/stu_add',methods=['GET'])
 def stu_add():
+    if 'login_type' not in session:
+        return render_template('login.html');
     new_account = request.args['new_account'];
     new_name = request.args['new_name'];
     new_sex = request.args['new_sex'];
     new_date = request.args['new_date'];
     new_class = request.args['new_class'];
     new_password = request.args['new_password'];
-    sql = "INSERT INTO student VALUES ('%s', '%s', '%s','%s','%s','%s' )"%(new_account,new_name,new_sex,new_date,new_class,new_password)
+
+    #class_name->class_id
     database = "database\Stu_Message_manger.db";
-    conn = sqlite3.connect(database)
+    conn = sqlite3.connect(database);
     cur = conn.cursor()
+    sql = "select id from class where name = '%s'"%(new_class);
+    cur.execute(sql);
+    new_class = cur.fetchall()[0][0];
+    sql = "INSERT INTO student VALUES ('%s', '%s', '%s','%s','%s','%s' )"%(new_account,new_name,new_sex,new_date,new_class,new_password)
+
+
     cur.execute(sql);
     conn.commit();
     cur.close();
     # 查询新的学生表
     cur = conn.cursor()
-    cur.execute("select * from student");
+    cur.execute("select student.id,student.name,student.sex,student.birthday,class.name,password from student,class where class.id = student.class_num;");
     result = cur.fetchall()  # 获得查询的结果
     cur.close()
     return render_template('manager_stu_list.html', account=session['account'], name=session['name'],sex=session['sex'], stu_table=result);
 
 @app.route('/to_add_teacher',methods=['GET'])
 def to_add_teacher():
+    if 'login_type' not in session:
+        return render_template('login.html');
     return render_template('add_teacher.html')
 
 @app.route('/tea_add',methods=['GET'])
 def tea_add():
+    if 'login_type' not in session:
+        return render_template('login.html');
     new_account = request.args['new_account'];
     new_name = request.args['new_name'];
     new_sex = request.args['new_sex'];
@@ -435,6 +575,8 @@ def tea_add():
 
 @app.route('/to_add_class',methods=['GET'])
 def to_add_class():
+    if 'login_type' not in session:
+        return render_template('login.html');
     database = "database\Stu_Message_manger.db"
     conn = sqlite3.connect(database)
     cur = conn.cursor()
@@ -443,6 +585,8 @@ def to_add_class():
     return render_template('add_class.html',teacher_name_list = teacher_name_list);
 @app.route('/class_add',methods=['GET'])
 def class_add():
+    if 'login_type' not in session:
+        return render_template('login.html');
     new_account = request.args['new_account'];
     new_name = request.args['new_name'];
     new_teacher = request.args['new_teacher'];
@@ -468,6 +612,8 @@ def class_add():
     return render_template('manager_class_list.html', account=session['account'], name=session['name'],sex=session['sex'], class_table=result);
 @app.route('/to_tea_course',methods=['GET'])
 def to_tea_class():
+    if 'login_type' not in session:
+        return render_template('login.html');
     tea_id = request.args['tea_id'];
     sql = "select course.id,course.name,teacher.name,course.max_num,course.stu_num,course.time,course.credit,course.start_term,course.class_type from course,teacher where course.teacher_id = teacher.id and teacher_id = '%s'"%(tea_id);
     database = "database\Stu_Message_manger.db";
@@ -479,6 +625,8 @@ def to_tea_class():
 
 @app.route('/course_detail',methods=['GET'])
 def class_detail():
+    if 'login_type' not in session:
+        return render_template('login.html');
     course_id = request.args['course_id'];
     sql = "select grade.id ,course.name,student.name,grade.grade from course,student,grade where grade.student_id = student.id and grade.class_id = course.id and course.id = '%s'"%(course_id);
     database = "database\Stu_Message_manger.db";
@@ -491,6 +639,8 @@ def class_detail():
 
 @app.route('/tea_change_grade',methods=['GET'])
 def tea_change_grade():
+    if 'login_type' not in session:
+        return render_template('login.html');
     course_id = request.args['course_id'];
     grade_id = request.args['grade_id'];
     new_grade = request.args['new_grade'];
